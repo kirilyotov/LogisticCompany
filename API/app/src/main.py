1c5b81abfb826df6a11759c8ec01557a533379f7
Kiril Yotov <kirilyotov@outlook.com>
2026-01-19 00:42:31 +0200

    routers and exception handler add

--- API/app/src/main.py
+++ API/app/src/main.py
@@ -1,6 +1,16 @@
+import logging
 from fastapi import FastAPI
+from API.app.src.api.v1.routers import api_router
+from API.app.src.core.exception_handlers import ExceptionHandlers
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+# Register Exception Handlers
+ExceptionHandlers.register(app)
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
